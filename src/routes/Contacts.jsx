1265ca691276de34c0c16494e98c8a3e7c8bf624
Thorben <38816229+tklein1801@users.noreply.github.com>
2021-03-20 01:17:13 +0100

    Get phone-numbers & contacts from rlrpg

--- src/routes/Contacts.jsx
+++ src/routes/Contacts.jsx
@@ -1,80 +1,76 @@
 import React, { Component } from "react";
 import Loader from "../components/Loader";
-import { Card, Row, Col, InputGroup, FormControl, Button, Modal, Form } from "react-bootstrap";
-import { faCreditCard, faPhone, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { Card, Row, Col, InputGroup, FormControl } from "react-bootstrap";
+import { faCreditCard, faPhone } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { SwipeableListItem } from "@sandstreamdev/react-swipeable-list";
-import "@sandstreamdev/react-swipeable-list/dist/styles.css";
+// Stylesheets
 import ReallifeRPG from "../ReallifeRPG";
-import { Contact } from "../DulliAG";
 
 export default class Contacts extends Component {
   constructor() {
     super();
     this.state = {
       loading: true,
-      insertModal: false,
     };
-    this.insertModal = React.createRef();
   }
 
-  handleOpen = () => this.setState({ insertModal: true });
+  _renderNoContent = (message) => {
+    return (
+      <div className="bg-light rounded p-3 mb-3">
+        <p className="text text-center font-weight-bold mb-0">{message}</p>
+      </div>
+    );
+  };
 
-  handleClose = () => this.setState({ insertModal: false });
+  _renderPhoneNumber = (phoneNumber) => {
+    return (
+      <div className="bg-light mb-3 rounded p-2" key={phoneNumber.phone}>
+        <Row>
+          <Col xs={12} md={12} lg={6} xl={6}>
+            <p className="text text-center font-weight-bold mb-0">
+              {phoneNumber.note === "default" ? "Standardnummer" : phoneNumber.note}
+            </p>
+          </Col>
+          <Col xs={12} md={12} lg={6} xl={6} className="border-right-2">
+            <p className="text text-center font-weight-bold mb-0">
+              <FontAwesomeIcon icon={faPhone} className="icon mr-2" />
+              {phoneNumber.phone}
+            </p>
+          </Col>
+        </Row>
+      </div>
+    );
+  };
 
-  async removeContact(contactId) {
-    const { displayedContacts } = this.state;
-    try {
-      const result = await new Contact().delete(contactId);
-      if (result.error === null) {
-        const newList = displayedContacts.filter((contact) => {
-          return contact.id !== contactId;
-        });
-        this.setState({ displayedContacts: newList });
-      } else {
-        console.error(result.error);
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  submitForm = async (event) => {
-    event.preventDefault();
-    const { profile, displayedContacts } = this.state;
-    const { phone, iban } = event.target.elements;
-    try {
-      const result = await new Contact().create(
-        profile.name,
-        profile.pid,
-        profile.avatar_full,
-        phone.value,
-        iban.value
-      );
-      if (result.error === null) {
-        // Display the inserted contact
-        displayedContacts.push({
-          id: result.inserted_id,
-          playerName: profile.name,
-          playerId: profile.pid,
-          avatarUrl: profile.avatar_full,
-          telNo: phone.value,
-          iban: iban.value,
-        });
-      } else {
-        console.error(result.error);
-      }
-    } catch (err) {
-      console.error(err);
-    }
-    this.handleClose();
+  _renderContact = (contact) => {
+    return (
+      <div className="bg-light mb-3 rounded p-2" key={contact.name}>
+        <Row>
+          <Col xs={12} md={12} lg={12} xl={12}>
+            <p className="text text-center font-weight-bold mb-0">{contact.name}</p>
+          </Col>
+          <Col xs={12} md={12} lg={6} xl={6} className="border-right-2">
+            <p className="text text-center font-weight-bold mb-0 mb-2">
+              <FontAwesomeIcon icon={faPhone} className="icon mr-2" />
+              {contact.number}
+            </p>
+          </Col>
+          <Col xs={12} md={12} lg={6} xl={6}>
+            <p className="text text-center font-weight-bold mb-0">
+              <FontAwesomeIcon icon={faCreditCard} className="icon mr-2" />
+              {contact.iban !== "" ? contact.iban : "Keine Angabe"}
+            </p>
+          </Col>
+        </Row>
+      </div>
+    );
   };
 
   filterContacts = (event) => {
     const { contacts } = this.state;
     const keyword = event.target.value;
     const result = contacts.filter((contact) => {
-      const playerName = contact.playerName.toLowerCase();
+      const playerName = contact.name.toLowerCase();
       return playerName.includes(keyword.toLowerCase());
     });
     this.setState({ displayedContacts: result });
@@ -83,21 +79,49 @@ export default class Contacts extends Component {
   async componentDidMount() {
     const apiKey = localStorage.getItem("@dag_apiKey");
     const profile = await new ReallifeRPG().getProfile(apiKey);
-    const contacts = await new Contact().getAll();
+    const phonebook = profile.data[0].phonebooks[0].phonebook;
+    const phoneNumbers = profile.data[0].phones;
     this.setState({
-      profile: profile.data[0],
-      contacts: contacts,
-      displayedContacts: contacts,
+      contacts: phonebook,
+      displayedContacts: phonebook,
+      phoneNumbers: phoneNumbers,
       loading: false,
     });
   }
 
   render() {
-    const { loading, profile, displayedContacts, insertModal } = this.state;
+    const { loading, displayedContacts, phoneNumbers } = this.state;
     return (
       <div className="contacts">
         <h1>Kontaktbuch</h1>
         <Row>
+          <Col xs={12} md={12} lg={6} xl={3}>
+            {loading ? (
+              <Card className="border-top shadow-md p-5">
+                <Loader />
+              </Card>
+            ) : (
+              <Card className="border-top shadow-md mb-3 mb-md-0">
+                <Card.Header className="bg-white border-bottom-0 pb-0">
+                  <InputGroup className="mb-3">
+                    <FormControl
+                      type="text"
+                      placeholder="Kontakt suchen"
+                      onChange={this.filterContacts}
+                    />
+                  </InputGroup>
+                </Card.Header>
+                <Card.Body className="pb-0 pt-0">
+                  {displayedContacts.length > 0
+                    ? displayedContacts.map((contact) => {
+                        return this._renderContact(contact);
+                      })
+                    : this._renderNoContent("Keine Kontakte gefunden")}
+                </Card.Body>
+              </Card>
+            )}
+          </Col>
+
           <Col xs={12} md={12} lg={6} xl={4}>
             {loading ? (
               <Card className="border-top shadow-md p-5">
@@ -106,169 +130,19 @@ export default class Contacts extends Component {
             ) : (
               <Card className="border-top shadow-md">
                 <Card.Header className="bg-white border-bottom-0 pb-0">
-                  <InputGroup className="mb-3">
-                    <FormControl
-                      type="text"
-                      placeholder="Kontakt suchen"
-                      onChange={this.filterContacts}
-                    />
-                    <InputGroup.Append>
-                      <Button variant="primary" className="px-3 rounded" onClick={this.handleOpen}>
-                        Eintragen
-                      </Button>
-                    </InputGroup.Append>
-                  </InputGroup>
+                  <Card.Title>Telefonnummern</Card.Title>
                 </Card.Header>
                 <Card.Body className="pb-0 pt-0">
-                  {displayedContacts.length > 0 ? (
-                    displayedContacts.map((contact, index) => {
-                      return profile.pid === contact.playerId ? (
-                        <SwipeableListItem
-                          key={index}
-                          swipeLeft={{
-                            content: (
-                              <div
-                                className="d-flex w-100 align-items-center justify-content-end px-3 mb-3 bg-danger text-white rounded"
-                                style={{ height: "calc(100% - 1rem)" }}
-                              >
-                                <FontAwesomeIcon
-                                  icon={faTrashAlt}
-                                  className="icon"
-                                  style={{ fontSize: "2rem" }}
-                                />
-                              </div>
-                            ),
-                            action: () => {
-                              this.removeContact(contact.id);
-                            },
-                          }}
-                        >
-                          <div className="bg-light mb-3 rounded p-2">
-                            <Row>
-                              <Col xs={12} md={12} lg={12} xl={12}>
-                                <p className="text text-center font-weight-bold">
-                                  <img
-                                    className="rounded-circle mr-2"
-                                    src={contact.avatarUrl}
-                                    alt="avatar"
-                                    style={{ width: "2.1rem", height: "auto" }}
-                                  />
-                                  {contact.playerName}
-                                </p>
-                              </Col>
-                              <Col xs={12} md={12} lg={6} xl={6} className="border-right-2">
-                                <p className="text text-center font-weight-bold mb-0 mb-2">
-                                  <FontAwesomeIcon icon={faPhone} className="icon mr-2" />
-                                  {contact.telNo}
-                                </p>
-                              </Col>
-                              <Col xs={12} md={12} lg={6} xl={6}>
-                                <p className="text text-center font-weight-bold mb-0">
-                                  <FontAwesomeIcon icon={faCreditCard} className="icon mr-2" />
-                                  {contact.iban}
-                                </p>
-                              </Col>
-                            </Row>
-                          </div>
-                        </SwipeableListItem>
-                      ) : (
-                        <div className="bg-light mb-3 rounded p-2" key={index}>
-                          <Row>
-                            <Col xs={12} md={12} lg={12} xl={12}>
-                              <p className="text text-center font-weight-bold">
-                                <img
-                                  className="rounded-circle mr-2"
-                                  src={contact.avatarUrl}
-                                  alt="avatar"
-                                  style={{ width: "2.1rem", height: "auto" }}
-                                />
-                                {contact.playerName}
-                              </p>
-                            </Col>
-                            <Col xs={12} md={12} lg={6} xl={6} className="border-right-2">
-                              <p className="text text-center font-weight-bold mb-0 mb-2">
-                                <FontAwesomeIcon icon={faPhone} className="icon mr-2" />
-                                {contact.telNo}
-                              </p>
-                            </Col>
-                            <Col xs={12} md={12} lg={6} xl={6}>
-                              <p className="text text-center font-weight-bold mb-0">
-                                <FontAwesomeIcon icon={faCreditCard} className="icon mr-2" />
-                                {contact.iban}
-                              </p>
-                            </Col>
-                          </Row>
-                        </div>
-                      );
-                    })
-                  ) : (
-                    <div className="bg-light rounded p-3 mb-3">
-                      <p className="text text-center font-weight-bold mb-0">
-                        Keinen Kontakt gefunden
-                      </p>
-                    </div>
-                  )}
+                  {phoneNumbers.length > 0
+                    ? phoneNumbers.map((phoneNumber) => {
+                        return this._renderPhoneNumber(phoneNumber);
+                      })
+                    : this._renderNoContent("Keine Telefonnummern gefunden")}
                 </Card.Body>
               </Card>
             )}
           </Col>
         </Row>
-
-        <Modal show={insertModal} onHide={this.handleClose} backdrop="static" size="md" centered>
-          <Modal.Header /*closeButton*/>
-            <Modal.Title id="contained-modal-title-vcenter">Kontakt erstellen</Modal.Title>
-          </Modal.Header>
-          <Form onSubmit={this.submitForm}>
-            <Modal.Body className="pb-0">
-              <Form.Group as={Row}>
-                <Form.Label column sm={2}>
-                  Name
-                </Form.Label>
-                <Col sm={10}>
-                  <Form.Control
-                    type="text"
-                    name="username"
-                    value={profile !== undefined ? profile.name : "Unbekannt"}
-                    disabled
-                  />
-                </Col>
-              </Form.Group>
-              <Form.Group as={Row}>
-                <Form.Label column sm={2}>
-                  Telefon
-                </Form.Label>
-                <Col sm={10}>
-                  <Form.Control
-                    type="number"
-                    pattern="numeric"
-                    name="phone"
-                    placeholder="Telefonnummer eintragen"
-                  />
-                </Col>
-              </Form.Group>
-              <Form.Group as={Row}>
-                <Form.Label column sm={2}>
-                  IBAN
-                </Form.Label>
-                <Col sm={10}>
-                  <Form.Control
-                    type="text"
-                    name="iban"
-                    value={profile !== undefined ? profile.bank_main[0].iban : "Unbekannt"}
-                  />
-                </Col>
-              </Form.Group>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant="white" className="px-3" onClick={this.handleClose}>
-                Abbrechen
-              </Button>
-              <Button variant="primary" className="px-3" type="submit">
-                Speichern
-              </Button>
-            </Modal.Footer>
-          </Form>
-        </Modal>
       </div>
     );
   }
