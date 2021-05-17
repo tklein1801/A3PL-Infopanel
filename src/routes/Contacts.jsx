e64e938fdff3d6432bc50c2a73b7dddc923b05c4
Thorben <38816229+tklein1801@users.noreply.github.com>
2021-05-17 17:10:51 +0200

    Fix phonebooks

--- src/routes/Contacts.jsx
+++ src/routes/Contacts.jsx
@@ -14,58 +14,6 @@ export default class Contacts extends Component {
     };
   }
 
-  _renderNoContent = (message) => {
-    return (
-      <div className="bg-light rounded p-3 mb-3">
-        <p className="text text-center font-weight-bold mb-0">{message}</p>
-      </div>
-    );
-  };
-
-  _renderPhoneNumber = (phoneNumber) => {
-    return (
-      <div className="bg-light mb-3 rounded p-2" key={phoneNumber.phone}>
-        <Row>
-          <Col xs={12} md={12} lg={6} xl={6}>
-            <p className="text text-center font-weight-bold mb-0">
-              {phoneNumber.note === "default" ? "Standardnummer" : phoneNumber.note}
-            </p>
-          </Col>
-          <Col xs={12} md={12} lg={6} xl={6} className="border-right-2">
-            <p className="text text-center font-weight-bold mb-0">
-              <FontAwesomeIcon icon={faPhone} className="icon mr-2" />
-              {phoneNumber.phone}
-            </p>
-          </Col>
-        </Row>
-      </div>
-    );
-  };
-
-  _renderContact = (contact) => {
-    return (
-      <div className="bg-light mb-3 rounded p-2" key={contact.name}>
-        <Row>
-          <Col xs={12} md={12} lg={12} xl={12}>
-            <p className="text text-center font-weight-bold mb-0">{contact.name}</p>
-          </Col>
-          <Col xs={12} md={12} lg={6} xl={6} className="border-right-2">
-            <p className="text text-center font-weight-bold mb-0 mb-2">
-              <FontAwesomeIcon icon={faPhone} className="icon mr-2" />
-              {contact.number}
-            </p>
-          </Col>
-          <Col xs={12} md={12} lg={6} xl={6}>
-            <p className="text text-center font-weight-bold mb-0">
-              <FontAwesomeIcon icon={faCreditCard} className="icon mr-2" />
-              {contact.iban !== "" ? contact.iban : "Keine Angabe"}
-            </p>
-          </Col>
-        </Row>
-      </div>
-    );
-  };
-
   filterContacts = (event) => {
     const { contacts } = this.state;
     const keyword = event.target.value;
@@ -79,48 +27,126 @@ export default class Contacts extends Component {
   async componentDidMount() {
     const apiKey = localStorage.getItem("@dag_apiKey");
     const profile = await new ReallifeRPG().getProfile(apiKey);
-    const phonebook = profile.data[0].phonebooks[0].phonebook;
-    const phoneNumbers = profile.data[0].phones;
+    var profileData = profile.data[0];
+    var phonebook = profileData.phonebooks[0].phonebook;
+    var phonebooks = profileData.phonebooks;
+    var phoneNumbers = profileData.phones;
+    phoneNumbers = phoneNumbers.filter((number) => number.disabled === 0);
     this.setState({
       contacts: phonebook,
       displayedContacts: phonebook,
+      phonebooks: phonebooks,
       phoneNumbers: phoneNumbers,
       loading: false,
     });
   }
 
   render() {
-    const { loading, displayedContacts, phoneNumbers } = this.state;
+    const { loading, displayedContacts, phonebooks, phoneNumbers } = this.state;
+
+    const Contact = ({ name, iban, phone }) => {
+      return (
+        <div className="bg-light mb-3 rounded p-2">
+          <Row>
+            <Col xs={12} md={12} lg={12} xl={12}>
+              <p className="text text-center font-weight-bold mb-0">{name}</p>
+            </Col>
+            <Col xs={12} md={12} lg={6} xl={6} className="border-right-2">
+              <p className="text text-center font-weight-bold mb-0 mb-2">
+                <FontAwesomeIcon icon={faPhone} className="icon mr-2" />
+                {phone}
+              </p>
+            </Col>
+            <Col xs={12} md={12} lg={6} xl={6}>
+              <p className="text text-center font-weight-bold mb-0">
+                <FontAwesomeIcon icon={faCreditCard} className="icon mr-2" />
+                {iban !== "" ? iban : "Keine Angabe"}
+              </p>
+            </Col>
+          </Row>
+        </div>
+      );
+    };
+
+    const PhoneNumber = ({ note, phone }) => {
+      return (
+        <div className="bg-light mb-3 rounded p-2">
+          <Row>
+            <Col xs={12} md={12} lg={6} xl={6}>
+              <p className="text text-center font-weight-bold mb-0">
+                {note === "default" ? "Standardnummer" : note}
+              </p>
+            </Col>
+            <Col xs={12} md={12} lg={6} xl={6} className="border-right-2">
+              <p className="text text-center font-weight-bold mb-0">
+                <FontAwesomeIcon icon={faPhone} className="icon mr-2" />
+                {phone}
+              </p>
+            </Col>
+          </Row>
+        </div>
+      );
+    };
+
+    const NoContent = ({ message }) => {
+      return (
+        <div className="bg-light rounded p-3 mb-3">
+          <p className="text text-center font-weight-bold mb-0">{message}</p>
+        </div>
+      );
+    };
+
     return (
       <div className="contacts">
         <h3 className="page-title">Kontaktbuch</h3>
         <Row>
-          <Col xs={12} md={12} lg={6} xl={4}>
-            {loading ? (
+          {loading ? (
+            <Col xs={12} md={12} lg={6} xl={4}>
               <Card className="border-top shadow-md p-5">
                 <Loader />
               </Card>
-            ) : (
-              <Card className="border-top shadow-md mb-3 mb-md-0">
-                <Card.Header className="bg-white border-bottom-0 pb-0">
-                  <InputGroup className="mb-3">
-                    <FormControl
-                      type="text"
-                      placeholder="Kontakt suchen"
-                      onChange={this.filterContacts}
-                    />
-                  </InputGroup>
-                </Card.Header>
-                <Card.Body className="pb-0 pt-0">
-                  {displayedContacts.length > 0
-                    ? displayedContacts.map((contact) => {
-                        return this._renderContact(contact);
-                      })
-                    : this._renderNoContent("Keine Kontakte gefunden")}
-                </Card.Body>
-              </Card>
-            )}
-          </Col>
+            </Col>
+          ) : (
+            phonebooks.map((book) => {
+              var side = book.side;
+              if (side === "CIV") {
+                side = "Zivilist";
+              } else if (side === "COP") {
+                side = "Polizei";
+              } else if (side === "EAST") {
+                side = "RAC";
+              } else if (side === "MEDIC" || side === "GUER") {
+                side = "Abramier";
+              } else {
+                side = "Unbekannt";
+              }
+              var contactList = book.phonebook;
+              return (
+                <Col xs={12} md={12} lg={6} xl={4}>
+                  <Card className="border-top shadow-md mb-3 mb-md-0">
+                    <Card.Header className="bg-white border-bottom-0 pb-0">
+                      <Card.Title>{side}</Card.Title>
+                    </Card.Header>
+                    <Card.Body className="pb-0 pt-0">
+                      {contactList.length > 0 ? (
+                        contactList.map((contact) => {
+                          return (
+                            <Contact
+                              phone={contact.number}
+                              iban={contact.iban}
+                              name={contact.name}
+                            />
+                          );
+                        })
+                      ) : (
+                        <NoContent message="Keine Kontakte gefunden" />
+                      )}
+                    </Card.Body>
+                  </Card>
+                </Col>
+              );
+            })
+          )}
 
           <Col xs={12} md={12} lg={6} xl={4}>
             {loading ? (
@@ -133,11 +159,27 @@ export default class Contacts extends Component {
                   <Card.Title>Telefonnummern</Card.Title>
                 </Card.Header>
                 <Card.Body className="pb-0 pt-0">
-                  {phoneNumbers.length > 0
-                    ? phoneNumbers.map((phoneNumber) => {
-                        return this._renderPhoneNumber(phoneNumber);
-                      })
-                    : this._renderNoContent("Keine Telefonnummern gefunden")}
+                  {phoneNumbers.length > 0 ? (
+                    phoneNumbers.map((phone, index) => {
+                      var note;
+                      switch (phone.note) {
+                        case "default":
+                          note = "Standardnummer";
+                          break;
+
+                        case "bought":
+                          note = "Gekauft";
+                          break;
+
+                        default:
+                          note = phone.note;
+                          break;
+                      }
+                      return <PhoneNumber note={note} phone={phone.phone} key={index} />;
+                    })
+                  ) : (
+                    <NoContent message="Keine Telefonnummern gefunden" />
+                  )}
                 </Card.Body>
               </Card>
             )}
