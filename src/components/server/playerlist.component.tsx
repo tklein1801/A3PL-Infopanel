fda67da33285abe0b4c4dc62696127874b8fd4a5
Thorben <38816229+tklein1801@users.noreply.github.com>
2024-05-04 19:30:56 +0200

    refactor: Increate column-width of playerlist

--- src/components/server/playerlist.component.tsx
+++ src/components/server/playerlist.component.tsx
@@ -16,7 +16,7 @@ export const Playerlist: React.FC<PlayerlistProps> = ({ server }) => {
       </Typography>
 
       <Grid container spacing={3}>
-        <Grid item xs={12} md={4}>
+        <Grid item xs={12} md={6}>
           {server.players.length > 0 ? (
             <Paper sx={{ p: 2 }}>
               {'side' in server ? (
